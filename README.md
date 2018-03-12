fb402e3feecdcd3391c5eabd7b02a2147d46f1c6
Juanjo Gamboa <juanjogam@gmail.com>
2018-03-12 15:31:35 +0100

    Updated package documentation

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # rospypavumeter
+
+## Overview
+
+Analog VU meter developed using the PulseAudio library, using a Python wrapper of the original C library. Composed by one node, vumeter_node, explained below. 
+
+## Run
+
+Example of a launcher for a vumeter node. For more information try to run the node using rosrun
+```sh
+$ rosrun rospypavumeter vumeter -h
+```
+For running using roslaunch
+```sh
+$ roslaunch rospypavumeter levels_monitor.launch robot:=<robot_name>
+```
+
+## ROS API
+
+### Nodes
+
+#### vumeter_node
+
+Analog VU meter connected to Linux sound system, PulseAudio, allowing any sound output (or "sink" in PulseAudio's nomenclature) to be monitored. The main PulseAudio API is asynchronous and callback based, and the documentation is primarly just an API reference. To help with volume meter style applications, PulseAudio even allows you to ask for peak level measurements, which means you can sample the monitor sink at a low frequency, with low CPU utilisation, but still producing a useful volume display. When this feature is used, each sample read indicates the peak level since the last sample. This program is designed to detect whatever sink is active in any moment in a dynamic manner, instead of previous implementations which have this parameter harcoded, and read the stream of audio coming from it, publishing through ROS protocol the detected peaks.  
+
+##### Published Topics
+
+`audioLevel`(*std_msgs/UInt8*):
+    Integer with the value of the peak received from the sink.
+
+##### Parameters
+
+`~meter_rate`(*int*, default: *5*):
+    Rate of audio checks.
+
+`~display_scale`(*int*, default: *0*):
+    For scaling purposes, not used.
+
+## LICENSE
+
+The license of the packages is custom LASR-UC3M (Licencia Académica Social Robotics Lab - UC3M), an open, non-commercial license which enables you to download, modify and distribute the code as long as you distribute the sources.  
+
+## ACKNOWLEDGEMENTS
+
+![RoboticsLab](http://ieee.uc3m.es/images/thumb/b/b6/Roboticslab_text_new.jpg/128px-Roboticslab_text_new.jpg)
+![UC3M](http://ieee.uc3m.es/images/thumb/6/6b/Logo_uc3m_letras.png/256px-Logo_uc3m_letras.png)
--- src/rospypavumeter/vumeter.py
+++ src/rospypavumeter/vumeter.py
@@ -17,19 +17,11 @@ from std_msgs.msg import UInt8
 # ros node name
 _DEFAULT_NAME = 'vumeter_node'
 
-# edit to match the sink
-#~ _SINK_NAME = 'alsa_output.pci-0000_00_1b.0.analog-stereo'
-#~ _METER_RATE = 200 #344
-#~ _MAX_SAMPLE_VALUE = 127
-#~ _DISPLAY_SCALE = 0
-#~ _MAX_SPACES = _MAX_SAMPLE_VALUE >> _DISPLAY_SCALE
-
-
 class PeakMonitor(object):
 
-    def __init__(self, sink_name, rate):
-        self.sink_name = sink_name
+    def __init__(self, rate):
         self.rate = rate
+        # This boolean controls the correct setup of the vumeter
         self.pa_state = False
         self.timestamp = datetime.now()
         self.timestamp_stream = datetime.now()
@@ -50,6 +42,8 @@ class PeakMonitor(object):
         # connection to Pulseaudio
         _mainloop = pa_threaded_mainloop_new()
         _mainloop_api = pa_threaded_mainloop_get_api(_mainloop)
+        # Creates the context, which multiplexes commands, data streams and events through a single channel
+        # It also sets the callback detecting any modifications in its state
         context = pa_context_new(_mainloop_api, 'peak_demo')
         pa_context_set_state_callback(context, self._context_notify_cb, None)
 
@@ -64,13 +58,36 @@ class PeakMonitor(object):
         pa_context_connect(context, None, 0, None)
         pa_threaded_mainloop_start(_mainloop)
 
+    ##
+    ## @brief      Iterator function. An iterator in Python is simply an object that can be iterated upon.
+    ##             An object which will return data, one element at a time. This function stores all the values 
+    ##             stored in the sample queue in an iterable object, a generator
+    ##
+    ## @param      self  The object
+    ##
+    ## @return     This function stores all the values 
+    ##             stored in the sample queue in an iterable object, a generator, which is returned
+    ##
     def __iter__(self):
         while True:
             yield self._samples.get()
 
+    ##
+    ## @brief      This callback triggers when it is perceive any change in the context state,
+    ##             in this case, upon inicialization
+    ##
+    ## @param      self     The object
+    ## @param      context  A context is the basic object for a connection to a PulseAudio server. 
+    ##                      It multiplexes commands, data streams and events through a single channel.
+    ## @param      _        This is a placeholder for userdata
+    ##
+    ## @return     Does not return any value
+    ##
     def context_notify_cb(self, context, _):
+        
         state = pa_context_get_state(context)
 
+        # When the context is ready
         if state == PA_CONTEXT_READY:
             rospy.loginfo("Pulseaudio connection ready...")
             # Connected to Pulseaudio. Now request that sink_info_cb
@@ -80,17 +97,122 @@ class PeakMonitor(object):
             o = pa_context_get_sink_info_list(context, self._sink_info_cb, None)
             pa_operation_unref(o)
 
-            pa_context_set_subscribe_callback(context, self._context_subscribe_cb, None)
+            # Create the subscription calllbacks in order to catch events related to modifications in 
+            # soundcards, sinks and sources. This events are stored in a mask. In this particular case
+            # the events selected are the ones related with the sink and the soundcards 
             mask = PA_SUBSCRIPTION_MASK_SINK | PA_SUBSCRIPTION_MASK_SINK_INPUT | PA_SUBSCRIPTION_MASK_CARD
+            pa_context_set_subscribe_callback(context, self._context_subscribe_cb, None)
             o = pa_context_subscribe(context, mask, self._context_success_cb, None)
             pa_operation_unref(o)
 
+        # In the case the context is unable to connect. Notifies through terminal.
         elif state == PA_CONTEXT_FAILED :
             rospy.loginfo("Connection failed")
 
+        # If the process has finished, it just notifies via terminal.
         elif state == PA_CONTEXT_TERMINATED:
             rospy.loginfo("Connection terminated")
 
+
+    ##
+    ## @brief      The callback function that is called when new data is
+    ##             available from the stream.
+    ##
+    ## @param      self    The object
+    ## @param      stream  An opaque stream for playback or recording.
+    ## @param      length  The length of the received data
+    ## @param      _       Placeholder for userdata
+    ##
+    ## @return     Does not return anything
+    ##
+    def stream_read_cb(self, stream, length, _):
+        #if datetime.now() - self.timestamp_stream > timedelta(days=0,seconds=0.2):
+    	data = c_void_p()
+    	pa_stream_peek(stream, data, c_ulong(length))
+    	data = cast(data, POINTER(c_ubyte))
+    	for i in xrange(length):
+    		# When PA_SAMPLE_U8 is used, samples values range from 128
+    		# to 255 because the underlying audio data is signed but
+    		# it doesn't make sense to return signed peaks.
+    		self._samples.put(data[i] - 128)
+    	self.timestamp_stream = datetime.now()
+    	pa_stream_drop(stream)
+
+
+    ##
+    ## @brief      Context specific callback function that is called whenever the state of the daemon changes.
+    ##
+    ## @param      self       The object
+    ## @param      context    The context
+    ## @param      event_typ  An integer indicating the event type
+    ## @param      idex       The id of the operation
+    ## @param      _          Placeholder for userdata
+    ##
+    ## @return     Does not return any value
+    ##
+    def context_subscribe_cb(self, context, event_typ, idex, _):
+        
+        event_type_masked = event_typ & PA_SUBSCRIPTION_EVENT_TYPE_MASK
+
+        # If the event is related with a sink input event. This event, specifically in conjuction with a
+        # sink change event, allows the system to initialize, and properly set the active sink.
+        # The vumeter will wait until this sequence of events happens, meaning that there is active sink 
+        # reproducing audio, and will choose this sink as the active sink.
+        if (event_typ & PA_SUBSCRIPTION_EVENT_FACILITY_MASK) == PA_SUBSCRIPTION_EVENT_SINK_INPUT:
+                    
+            if event_type_masked == PA_SUBSCRIPTION_EVENT_NEW and self.pa_state == False:
+                # Connect this event with the Sink Change event, if they happen in sequence, the system becomes
+                # active from its initial suspended state and chooses the active sink.
+                self.timestamp = datetime.now()
+                
+        # This statement catches events related with the sinks
+        # In this case the event catched is meant to occcur after the Sink Input event, in the system initial state,
+        # waking the system up and choosing an active sink. 
+        elif (event_typ & PA_SUBSCRIPTION_EVENT_FACILITY_MASK) == PA_SUBSCRIPTION_EVENT_SINK:
+                   
+            if event_type_masked == PA_SUBSCRIPTION_EVENT_CHANGE and self.pa_state == False:
+                #print "Changes detected in the Sink. Comparing the timestamp..."
+                if datetime.now() - self.timestamp < timedelta(days=0,seconds=2):
+                    #print "Enough time has passed since the timestamp was recorded"
+                    pa_stream_disconnect(self.pa_stream)
+                    o = pa_context_get_sink_info_list(context, self._sink_info_cb, None)
+                    pa_operation_unref(o)
+
+        # This statement catches any modifications made to the soundcards as additions, removals, etc
+        # Card-related events cannot occur at the same time, what makes the system easier to be controlled
+        elif (event_typ & PA_SUBSCRIPTION_EVENT_FACILITY_MASK) == PA_SUBSCRIPTION_EVENT_CARD:
+            #print "Card event detected"
+            pa_stream_disconnect(self.pa_stream)
+            time.sleep(1)
+            o = pa_context_get_sink_info_list(context, self._sink_info_cb, None)
+            pa_operation_unref(o)
+   
+    ##
+    ## @brief      A generic callback for operation completion. 
+    ##             Has to be defined in order to the context to be able to subscribe  
+    ##
+    ## @param      self       The object
+    ## @param      context    The context
+    ## @param      succedded  Integer indicating the success of the operation
+    ## @param      _          Placeholder for userdata
+    ##
+    ## @return     Does not returns
+    ##
+    def context_success_cb(self, context, succedded, _):
+        rospy.loginfo('Subscribe operation completed')
+
+    ##
+    ## @brief      Get some information about a sink. If the sink is running, sets it as the active sink, 
+    ##             connecting it to a stream 
+    ##
+    ## @param      self         The object
+    ## @param      context      The context
+    ## @param      sink_info_p  Structure that stores info about sinks
+    ## @param      _            Placeholder for eol integer value
+    ## @param      __           Placeholder for userdata
+    ##
+    ## @return     As a callback, does not returns
+    ##
     def sink_info_cb(self, context, sink_info_p, _, __):
         if not sink_info_p:
             return
@@ -112,6 +234,7 @@ class PeakMonitor(object):
             self.pa_stream = pa_stream_new(context, "peak detect demo", self.samplespec, None)
             self.pa_state = True
 
+            # Setting the stream reading and recording callbacks
             pa_stream_set_read_callback(self.pa_stream,
                                         self._stream_read_cb,
                                         sink_info.index)
@@ -120,77 +243,33 @@ class PeakMonitor(object):
                                      None,
                                      PA_STREAM_PEAK_DETECT)
 
-    def stream_read_cb(self, stream, length, index_incr):
-        #if datetime.now() - self.timestamp_stream > timedelta(days=0,seconds=0.2):
-    	data = c_void_p()
-    	pa_stream_peek(stream, data, c_ulong(length))
-    	data = cast(data, POINTER(c_ubyte))
-    	for i in xrange(length):
-    		# When PA_SAMPLE_U8 is used, samples values range from 128
-    		# to 255 because the underlying audio data is signed but
-    		# it doesn't make sense to return signed peaks.
-    		self._samples.put(data[i] - 128)
-    	self.timestamp_stream = datetime.now()
-    	pa_stream_drop(stream)
-
-    def context_subscribe_cb(self, context, event_typ, idex, __):
-        #print "The event is ", event_typ
-        #print "The id is ", idex
-        event_type_masked = event_typ & PA_SUBSCRIPTION_EVENT_TYPE_MASK
-
-        if (event_typ & PA_SUBSCRIPTION_EVENT_FACILITY_MASK) == PA_SUBSCRIPTION_EVENT_SINK_INPUT:
-            #print "Sink Input Event"
-            if event_type_masked == PA_SUBSCRIPTION_EVENT_NEW and self.pa_state == False:
-                #print "SIA event detected. Proceeding to get the timestamp"
-                self.timestamp = datetime.now()
-                
-            # elif event_type_masked == PA_SUBSCRIPTION_EVENT_REMOVE:
-            #     "SIR event detected. Proceeding to close the stream."
-            #     pa_stream_disconnect(self.pa_stream)
-                
-        # This statement catches events related with the sinks
-        # In this case the event catched is meant to occcur after the Sink Input event and in the system initial state
-        elif (event_typ & PA_SUBSCRIPTION_EVENT_FACILITY_MASK) == PA_SUBSCRIPTION_EVENT_SINK:
-            #print "Sink event catched"
-            # if event_type_masked in (PA_SUBSCRIPTION_EVENT_NEW, PA_SUBSCRIPTION_EVENT_REMOVE):                    
-            if event_type_masked == PA_SUBSCRIPTION_EVENT_CHANGE and self.pa_state == False:
-                #print "Changes detected in the Sink. Comparing the timestamp..."
-                if datetime.now() - self.timestamp < timedelta(days=0,seconds=2):
-                    #print "Enough time has passed since the timestamp was recorded"
-                    pa_stream_disconnect(self.pa_stream)
-                    o = pa_context_get_sink_info_list(context, self._sink_info_cb, None)
-                    pa_operation_unref(o)
-
-        # This statement catches any modifications made to the soundcards as additions, removals, etc
-        # Card-related events cannot occur at the same time, what makes the system easier to control
-        elif (event_typ & PA_SUBSCRIPTION_EVENT_FACILITY_MASK) == PA_SUBSCRIPTION_EVENT_CARD:
-            #print "Card event detected"
-            pa_stream_disconnect(self.pa_stream)
-            time.sleep(1)
-            o = pa_context_get_sink_info_list(context, self._sink_info_cb, None)
-            pa_operation_unref(o)
-
-            # if event_type_masked == PA_SUBSCRIPTION_EVENT_NEW:# and self.pa_state == False:
-            #     print "Card added"   
-            # elif event_type_masked == PA_SUBSCRIPTION_EVENT_REMOVE:
-            #     print "Card removed"
-            # elif event_type_masked == PA_SUBSCRIPTION_EVENT_CHANGE:
-            #     print "Card changed"
-
-    def context_success_cb(self, context, succedded, _):
-        rospy.loginfo('Subscribe operation completed')
 
 def _init_node(node_name):
-	"""Common routines for a start node."""
+	"""
+    Common routines for a start node inside ROS environment
+    
+    @param      node_name  The node name
+    
+    @return     Does not return any value
+    """
 	rospy.init_node(node_name)
 	rospy.loginfo("Initializing {} Node".format(rospy.get_name()))
 
-def _audio_level_publisher(_SINK_NAME, _METER_RATE, _MAX_SAMPLE_VALUE, _DISPLAY_SCALE):
+##
+## @brief      This function publishes the peaks detected by the vumeter through ROS communication protocol
+##
+## @param      _METER_RATE        The meter rate
+## @param      _MAX_SAMPLE_VALUE  The max sample value
+## @param      _DISPLAY_SCALE     The display scale
+##
+## @return     
+##
+def _audio_level_publisher(_METER_RATE, _MAX_SAMPLE_VALUE, _DISPLAY_SCALE):
 
     audioLevelPublisher = rospy.Publisher('audioLevel', UInt8, queue_size=70)
     rate = rospy.Rate(_METER_RATE)
 	
-    monitor = PeakMonitor(_SINK_NAME, _METER_RATE)
+    monitor = PeakMonitor(_METER_RATE)
 	
     while not rospy.is_shutdown():
 		
@@ -210,13 +289,12 @@ if __name__ == '__main__':
 	
     _init_node(_DEFAULT_NAME)
 
-    SINK_NAME = rospy.get_param("sink_name")
     METER_RATE = rospy.get_param("meter_rate")
     MAX_SAMPLE_VALUE = rospy.get_param("max_sample_value")
     DISPLAY_SCALE = rospy.get_param("display_scale")
 
     try:
-		_audio_level_publisher(SINK_NAME, METER_RATE, MAX_SAMPLE_VALUE, DISPLAY_SCALE)
+		_audio_level_publisher(METER_RATE, MAX_SAMPLE_VALUE, DISPLAY_SCALE)
         
     except rospy.ROSInterruptException:
 		pass
